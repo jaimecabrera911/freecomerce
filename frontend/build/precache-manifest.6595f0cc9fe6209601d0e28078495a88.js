4a7cf660eeff88d6d75979aae076c6af91e6ed9a
jaime.cabrera <cabrerajaimeandres@gmail.com>
2020-10-29 01:01:01 -0500

    Version Anterior

---
+++ frontend/build/precache-manifest.6595f0cc9fe6209601d0e28078495a88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1182e1ab2ca4e7b7f59fcf0194165b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22c83f00f74a9b9b1d34",
+    "url": "/static/css/main.e8b138b8.chunk.css"
+  },
+  {
+    "revision": "b639c79332b3cc29d371",
+    "url": "/static/js/2.e0b289b8.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/static/js/2.e0b289b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c83f00f74a9b9b1d34",
+    "url": "/static/js/main.74239110.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
